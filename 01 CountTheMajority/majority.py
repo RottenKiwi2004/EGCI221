b9df6cd420a67ca99ad899945d6823d4f6ef7b68
thanakrit381 <markkittiphat@gmail.com>
2024-01-15 11:04:11 +0700

    Fixed edge cases
    Sometimes the remaining element might not be the majority. We need to loop over the array again to check if it is actually the majority.

--- 01 CountTheMajority/majority.py
+++ 01 CountTheMajority/majority.py
@@ -1,6 +1,6 @@
 from random import randint as rand
 
-# This is used for randomly generate the data array and it is not used in the actual algorithm
+# This is used for randomly generate the data array. It is not used in the actual algorithm
 numOfDistinctElements = 5000
 numOfElements = 200
 
@@ -12,7 +12,9 @@ print(f"Selected Majority: {selectedMajority}")
 data = [selectedMajority if rand(0, 2) else rand(1, numOfDistinctElements)
         for _ in range(numOfElements)]
 
-# Case that guaranteed to has no majority
+# data = [1, 1, 2, 3, 2]
+
+# Case that guaranteed to have no majority
 # selectedMajority = None
 # data = [i for i in range(numOfElements)]
 
@@ -26,18 +28,17 @@ print(data)
 # The real algorithm is inside this function
 def findMajority(dataArr: list):
 
-    # There are two cases for picking a pivot
+    # There are two cases for picking a subject
     # 1. It is the majority
     # 2. It is NOT the majority
 
     # Imagine the voting scenario where each person got 1 score
-    # Instead of counting how many votes our pivot got, we count how much our pivot got more than all others combined
-    # If the number is <= 0, then the chosen pivot is not our majority
-    # The nice thing about this approach is that if we iterate through the array, at any given point we will be able to
-    # tell if the pivot is or is not our majority by just checking the number.
+    # Instead of counting how many votes our subject got,
+    # we deduct a point from our subject when we find votes that go to others
+    # If the number is <= 0, then the chosen subject has not enough potential to be the majority
 
-    # Choose the first element as our pivot
-    pivot = dataArr[0]                          # Assignment: O(1)
+    # Choose the first element as our subject
+    subject = dataArr[0]                        # Assignment: O(1)
 
     # Currently it is as popular as others
     score = 0                                   # Assignment: O(1)
@@ -45,32 +46,58 @@ def findMajority(dataArr: list):
     n = len(dataArr)                            # len(): O(1) ; Assignment: O(1)
     # Loop through each element in the array
     for i in range(n):                          # Loop for n elements
-        d = dataArr[i]                          # Assignment: O(1)
-        # If the vote goes to our pivot
-        if d == pivot:                          # Condition: O(1)
-            # Our pivot gets more popular, and it might be the majority, increase the score
+        # If the vote goes to our subject
+        if dataArr[i] == subject:               # Condition: O(1)
+            # Our subject gets more popular, and it might be the majority, increase the score
             score += 1                          # Addition: O(1)
         # If the vote goes to someone else
         else:
-            # Our pivot has more competitor, decrease the score
+            # Our subject has more competitor, decrease the score
             score -= 1                          # Subtraction: O(1)
 
         # If score is 0, that's a tie.
-        # Choose the next one to be a new pivot because last one is not as popular as it should be.
+        # Choose the next one to be a new subject because previous one is not as popular as it should be.
         if score == 0 and i != n - 1:           # Condition: O(1)
-            pivot = dataArr[i + 1]              # Assignment: O(1)
+            subject = dataArr[i + 1]            # Assignment: O(1)
 
         # So each loop takes O(1)
     # Looping for n elements will take O(n)
 
+    # If the score cancels out, then it is clear that there is no potential subject. Thus, no majority, return None
     if score == 0:                              # Condition: O(1)
         return None                             # Return: O(1)
 
-    return pivot                                # Return: O(1)
+    # Here we have our potential subject that might be the majority
+    # But there are cases that our subject might not be the majority
+    # For example: [1, 1, 2, 3, 2] --> Our subject is 2, but it is not the majority
 
-    # Time complexity: O(n) + O(1) => O(n)
+    # So we have to loop over the array to check if the subject is actually the majority
+
+    subjectCount = 0                            # Assignment: O(1)
+
+    for element in dataArr:                     # Loop for n elements
+
+        if subject == element:                  # Condition: O(1)
+            subjectCount += 1                   # Assignment: O(1)
+
+        # Each loop takes O(1)
+
+    # Looping for n elements will take O(n)
+
+    # If our potential subject occurs more than half of the size of the array, then it is the majority.
+    if subjectCount > n // 2:                   # Condition: O(1)
+        return subject                          # Return: O(1)
+
+    # Otherwise, there is no majority in this array
+    return None                                 # Return: O(1)
+
+    # Time complexity:  O(n) + O(n) + ( O(1) + O(1) + ... + O(1) ) => O(n)
+    #                        ^                      ^
+    #                        |                      |
+    #                        |    Any other operation outside 2 main loops are lots of O(1) combined which is still O(1)
+    #                        |
+    #                   2( O(n) ) here from 2 loops
 
 
 majority = findMajority(data)
 print(f"Majority: {majority}")
-print(f"Correct answer? {majority == selectedMajority}")  # Only for
